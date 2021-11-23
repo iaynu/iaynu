312ddf89f3549f78c8e39a0ce9d0cc884003c612
revoll5 <revoll5@naver.com>
2021-11-23 23:28:56 +0900

    C Programming 7th project 'hw7.c'

---
+++ hw7.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+# include <math.h>
+
+int main(void)
+{
+	double q, w, e, r, t;
+	double result = 0;
+
+	printf("Enter 5 real numbers: ");
+	scanf_s("%lf %lf %lf %lf %lf", &q, &w, &e, &r, &t);
+
+
+	//아직 포인터에 대해서 잘 이해가 안갑니다. 공부를 더 해봐야겠습니다.
+
+ 
+
+}
